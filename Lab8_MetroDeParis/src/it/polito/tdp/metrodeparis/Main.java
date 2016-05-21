7c4ecd7b1fe733c2a05b198f5326cdf0bfa0b84e
AssiaP <mariaassunta.palmieri@gmail.com>
2016-05-21 17:30:06 +0200

    Fine Esercizio 1

--- Lab8_MetroDeParis/src/it/polito/tdp/metrodeparis/Main.java
+++ Lab8_MetroDeParis/src/it/polito/tdp/metrodeparis/Main.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.metrodeparis;
 	
+import it.polito.tdp.Lab8_MetroDeParis.Model.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,11 +12,18 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MetroDeParis.fxml"));
-			Scene scene = new Scene(root,400,400);
+			
+			FXMLLoader loader  = new FXMLLoader(getClass().getResource("MetroDeParis.fxml"));
+			
+			BorderPane root = (BorderPane)loader.load();
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			Model m= new Model();
+			MetroDeParisController controller = loader.getController();
+			controller.setModel(m);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- Lab8_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
+++ Lab8_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
@@ -1,5 +1,76 @@
 package it.polito.tdp.metrodeparis;
 
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.Lab8_MetroDeParis.Model.Model;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.TextArea;
+
 public class MetroDeParisController {
-	
+	private Model model = new Model();
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ChoiceBox<String> cbStazionePartenza;
+
+    @FXML
+    private ChoiceBox<String> cbStazioneArrivo;
+    
+    @FXML
+    private Button btnCalcolaDistanza;
+
+    @FXML
+    private TextArea txtArea;
+    
+	public void setModel(Model model) {
+		
+		this.model=model;
+		model.generaGrafo();
+		cbStazionePartenza.getItems().addAll(model.elencoNomifermate());
+        cbStazioneArrivo.getItems().addAll(model.elencoNomifermate());
+	}
+    @FXML
+    void doCalcolaDistanza(ActionEvent event) {
+
+    	
+    	String s1 = cbStazionePartenza.getValue();
+    	String s2 = cbStazioneArrivo.getValue();
+    	List<String> lista = new LinkedList<>();
+    	if(s1!=null && s2!=null){
+    		lista=model.getCammino(s1, s2);
+    	}
+    	int tempoTotaleInSecondi = (int) model.tempoPercorrenzaStimato();
+		int ore = tempoTotaleInSecondi / 3600;
+		int minuti = (tempoTotaleInSecondi % 3600) / 60;
+		int secondi = tempoTotaleInSecondi % 60;
+		String timeString = String.format("%02d:%02d:%02d", ore, minuti, secondi);
+    	txtArea.setText("Percorso: "+lista.toString()+"\n\nTempo di percorrenza stimato: "+timeString);
+    	
+    }
+
+    @FXML
+    void initialize() {
+        assert cbStazionePartenza != null : "fx:id=\"cbStazionePartenza\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert cbStazioneArrivo != null : "fx:id=\"cbStazioneArrivo\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert btnCalcolaDistanza != null : "fx:id=\"btnCalcolaDistanza\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert txtArea != null : "fx:id=\"txtArea\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+     
+    }
+
+
 }
